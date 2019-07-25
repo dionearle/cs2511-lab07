d1e1eb66895fa04a28c8925b251315499c749fc2
Dion Earle <dion.earle@gmail.com>
2019-07-25 16:55:49 +1000

    Implemented backend of game using boolean array

--- src/unsw/automata/GameOfLife.java
+++ src/unsw/automata/GameOfLife.java
@@ -10,26 +10,89 @@ package unsw.automata;
  *
  */
 public class GameOfLife {
+	
+	boolean array[][];
 
     public GameOfLife() {
-        // TODO At the start all cells are dead
+		array = new boolean[10][10];
     }
 
     public void ensureAlive(int x, int y) {
-        // TODO Set the cell at (x,y) as alive
+    	array[x][y] = true;
     }
 
     public void ensureDead(int x, int y) {
-        // TODO Set the cell at (x,y) as dead
+    	array[x][y] = false;
     }
 
     public boolean isAlive(int x, int y) {
-        // TODO Return true if the cell is alive
-        return false;
+    	if (array[x][y] == true) {
+    		return true;
+    	} else {
+    		return false;
+    	}
     }
-
+    
     public void tick() {
-        // TODO Transition the game to the next generation.
-    }
 
+    	// determine number of rows and columns in board (10 x 10)
+        int rows = array.length;
+        int cols = array[0].length;
+
+        // Create a copy of the board
+        boolean[][] copyBoard = new boolean[rows][cols];
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                copyBoard[row][col] = array[row][col];
+            }
+        }
+
+        // Now we go through each cell of the board
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+
+                // For each cell, we determine the number of live neighbours
+                int liveNeighbours = 0;
+                
+                int a = row - 1;
+    			for (int counter = 0; counter < 3; counter++) {
+    				
+    				int b = col - 1;
+    				for (int innerCounter = 0; innerCounter < 3; innerCounter++) {
+        					
+        					if (a < 0) {
+        						a = 9;
+        					} else if (a > 9) {
+        						a = 0;
+        					}
+        					
+        					if (b < 0) {
+        						b = 9;
+        					} else if (b > 9) {
+        						b = 0;
+        					}
+        					
+        					if (a == row && b == col) {
+        						b++;
+        						continue;
+        					}
+        					
+        					// if this neighbour is alive, we add to the count of live neighbours
+        					if (copyBoard[a][b] == true) {
+        						liveNeighbours++;
+        					}
+        					b++;
+    				}
+    				a++;
+    			}
+
+                // Testing the rules to see whether we change the current cell's state
+                if ((copyBoard[row][col] == true) && (liveNeighbours < 2 || liveNeighbours > 3)) {
+                    array[row][col] = false;
+                } else if (copyBoard[row][col] == false && liveNeighbours == 3) {
+                    array[row][col] = true;
+                }
+            }
+        }
+    }
 }
